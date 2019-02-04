43325bf45d93a1e24afbac86b82315017756f30a
Andy Newton <andy@vitri.ltd>
2019-02-04 14:10:58 +0000

    OLCS-22916 - ICW Creates a bilateral application Bugfix to allow total permits on form to == total vehicle auth, not be -1

--- module/Olcs/assets/js/inline/forms/irhp-application.js
+++ module/Olcs/assets/js/inline/forms/irhp-application.js
@@ -5,7 +5,7 @@ $(function () {
         $("input[name*='permitsRequired']").each(function () {
             totalPermits += parseInt($(this).val(), 10);
         });
-        if (totalPermits >= parseInt($("#numVehicles").val(), 10)) {
+        if (totalPermits > parseInt($("#numVehicles").val(), 10)) {
             $(".actions-container")
                 .addClass("validation-wrapper")
                 .prepend('<p class="error__text jserrormsg">Total Permits Required on this page may not exceed total vehicle authorisation</p>');
