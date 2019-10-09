ce3d98fceac13dc3560415aa220b85fe6bef992f
Shaun Hare <shaun.hare@dvsa.gov.uk>
2019-10-09 16:56:10 +0100

    removed request logger from webdav branch

--- public/index.php
+++ public/index.php
@@ -19,8 +19,6 @@ if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['RE
 // Setup autoloading
 require 'init_autoloader.php';
 
-include_once 'RequestLogger.php';
-(new  \RequestLogger())->execute();
 
 // Run the application!
 Zend\Mvc\Application::init(require 'config/application.config.php')->run();
