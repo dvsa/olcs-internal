692fbb88afd43eb4ade608390cb7c9c79067aa46
Jamie_gordon_1 <jamie.gordon@dvsa.gov.uk>
2023-12-04 16:33:46 +0000

    Organisation form list service

---
+++ module/Olcs/src/Service/Data/Organisation.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Olcs\Service\Data;
+
+use Common\Exception\DataServiceException;
+use Common\Service\Data\AbstractListDataService;
+use Dvsa\Olcs\Transfer\Query as TransferQry;
+
+/**
+ * Class Category
+ *
+ * @package Olcs\Service\Data
+ */
+class Organisation extends AbstractListDataService
+{
+
+    /**
+     * Fetch list data
+     *
+     * @param array $context Parameters
+     *
+     * @return array
+     * @throw DataServiceException
+     */
+    public function fetchListData($context = null)
+    {
+        $data = (array)$this->getData('categories');
+
+        if (0 !== count($data)) {
+            return $data;
+        }
+
+        $params = array_filter(
+            [
+                
+            ]
+        );
+
+        $response = $this->handleQuery(
+            TransferQry\Category\GetList::create($params)
+        );
+
+        if (!$response->isOk()) {
+            throw new DataServiceException('unknown-error');
+        }
+
+        $result = $response->getResult();
+
+       // $this->setData('categories', (isset($result['results']) ? $result['results'] : null));
+
+        //return $this->getData('categories');
+    }
+
+    
+}
